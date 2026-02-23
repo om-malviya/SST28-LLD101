3a73cd7d57d3d643b73d29f27a942603bd384bfd
Om Malviya <ommalviya@Oms-MacBook-Air-3.local>
2026-02-23 10:41:18 +0530

    Ex3 Complete

---
+++ SOLID/Ex3/src/AttendanceRule.java
@@ -0,0 +1,7 @@
+public class AttendanceRule implements EligibilityRule {
+    @Override
+    public String check(StudentProfile s) {
+        if (s.attendancePct < 75) return "attendance below 75";
+        return null;
+    }
+}
